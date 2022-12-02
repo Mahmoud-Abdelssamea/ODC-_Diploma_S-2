f81708dfd2c8f502d66f70829e78b9eab0019980
Mahmoud Awwad <mabdelssamea@gmail.com>
2022-12-02 22:15:36 +0200

    use class instead of function

--- index.js
+++ index.js
@@ -1,114 +1,130 @@
 const form = document.querySelector("#userData");
 const ul = document.querySelector("#list");
 
-// Get the data from user
-const getDataFromUser = (event) => {
-  const username = event.target.username.value;
-  const age = event.target.age.value;
-  const status = event.target.status.checked;
-  const id = Date.now();
-
-  return { username, age, status, id };
-};
-
-// save the user data to local storage
-const writeInLocalStorage = (data) => {
-  localStorage.setItem("users", JSON.stringify(data));
-};
-
-// read all users from localstorage
-const readFromLocalStorage = (dataType = "json", storageKey = "users") => {
-  if (dataType == "json") {
-    try {
-      const data = JSON.parse(localStorage.getItem(storageKey));
-      if (!data) throw Error();
-      return data;
-    } catch (error) {
-      return [];
-    }
+class User {
+  // Get data from form
+  constructor(event) {
+    this.username = event.target.username.value;
+    this.age = event.target.age.value;
+    this.status = event.target.status.checked;
+    this.id = Date.now();
   }
-};
 
-// delete fuction
-const removeUserFromStorage = (userId) => {
-  const updatedUsersList = allUsers.filter((user) => user.id != userId);
-  writeInLocalStorage(updatedUsersList);
-};
+  // Read Data from local Storage
+  static readFromLocalStorage = function (
+    dataType = "json",
+    storageKey = "users"
+  ) {
+    if (dataType == "json") {
+      try {
+        const data = JSON.parse(localStorage.getItem(storageKey));
+        if (!data) throw Error();
+        return data;
+      } catch (error) {
+        return [];
+      }
+    }
+  };
 
-// Update user status
-const updateStatus = (userId) => {
-  allUsers.forEach((user) => {
-    if (user.id == userId) user.status = !user.status;
-  });
-  writeInLocalStorage(allUsers);
-};
+  // write Data from local storage
+  static writeInLocalStorage = (data) => {
+    localStorage.setItem("users", JSON.stringify(data));
+  };
 
-// create button
-const btn = (btnName) => {
-  const btn = document.createElement("button");
-  btn.className = btnName;
-  btn.innerText = btnName;
-  return btn;
-};
+  // remove user data
+  static removeUserFromStorage = (userId) => {
+    const updatedUsersList = allUsers.filter((user) => user.id != userId);
+    writeInLocalStorage(updatedUsersList);
+  };
 
-// create li element
-const li = (user) => {
-  const li = document.createElement("li");
-  const name = document.createElement("span");
-  const age = document.createElement("span");
-  const status = document.createElement("button");
-  li.setAttribute("user_id", user.id);
-  name.innerText = `Name: ${user.username}`;
-  age.innerText = `, Age: ${user.age}`;
-  status.innerText = user.status ? " active " : " inactive";
-  status.className = "update";
+  // update status for user
+  static updateStatus = (userId) => {
+    allUsers.forEach((user) => {
+      if (user.id == userId) user.status = !user.status;
+    });
+    writeInLocalStorage(allUsers);
+  };
 
-  const deleteBtn = btn("delete");
+  // create li
+  static li = function (user) {
+    const li = document.createElement("li");
+    li.classList.add("item");
+    const name = document.createElement("span");
+    const age = document.createElement("span");
+    const status = document.createElement("button");
+    li.setAttribute("user_id", user.id);
+    name.innerText = `Name: ${user.username}`;
+    age.innerText = `, Age: ${user.age}`;
+    status.innerText = user.status ? " active " : " inactive";
+    status.className = "update";
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "delete";
+    deleteBtn.innerText = "delete";
 
-  li.appendChild(name);
-  li.appendChild(age);
-  li.appendChild(status);
-  li.appendChild(deleteBtn);
-  ul.appendChild(li);
-};
+    li.appendChild(name);
+    li.appendChild(age);
+    li.appendChild(status);
+    li.appendChild(deleteBtn);
+    ul.appendChild(li);
+  };
+}
 
-// read data from local storage
-let allUsers = readFromLocalStorage();
+let allUsers = User.readFromLocalStorage();
 
-// create an event listener to submit a new user
+allUsers.forEach((user) => User.li(user));
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const newUserData = getDataFromUser(e);
 
-  // add new user to all users list
-  allUsers.push(newUserData);
+  // create new user
+  const newUser = new User(e);
+  console.log(newUser);
 
-  // add new list of users to localstorage
-  writeInLocalStorage(allUsers);
+  //clean inputs
+  e.target.elements.age.value = "";
+  e.target.elements.username.value = "";
 
-  allUsers = readFromLocalStorage();
+  //save new user in localstorage
+  allUsers.push(newUser);
+  User.writeInLocalStorage(allUsers);
 
-  location.reload();
+  // get all users
+  allUsers = User.readFromLocalStorage();
+
+  // show data in browser
+  ul.innerHTML = "";
+  allUsers.forEach((user) => User.li(user));
 });
 
-//--- show all users in list
-allUsers.forEach((user) => {
-  li(user);
-});
-
-// delete
+// Delete user
 ul.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete")) {
-    removeUserFromStorage(e.target.parentElement.getAttribute("user_id"));
-    e.target.parentElement.remove();
+    const user_id = e.target.parentElement.getAttribute("user_id");
+    allUsers = allUsers.filter((user) => String(user.id) !== user_id);
   }
+
+  User.writeInLocalStorage(allUsers);
+  ul.innerHTML = "";
+  allUsers.forEach((user) => User.li(user));
 });
 
-// update the user status
+// Change status
 
 ul.addEventListener("click", (e) => {
+  e.preventDefault();
   if (e.target.classList.contains("update")) {
-    updateStatus(e.target.parentElement.getAttribute("user_id"));
+    const user_id = e.target.parentElement.getAttribute("user_id");
+    // filter and modify the status
+    allUsers = allUsers.map((user) => {
+      if (String(user.id) === user_id) {
+        return { ...user, status: !user.status };
+      } else {
+        return user;
+      }
+    });
   }
-  location.reload();
+  // update local storage
+  User.writeInLocalStorage(allUsers);
+  ul.innerHTML = "";
+  allUsers.forEach((user) => User.li(user));
 });
